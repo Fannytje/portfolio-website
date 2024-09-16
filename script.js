f997cf34d561d0a17a0605596f3238cee902c0b8
Fannytje <103598821+Fannytje@users.noreply.github.com>
2024-09-16 21:23:21 +0200

    Adjustments About

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
-  const content = document.getElementById('toggleContent');
-  const arrowIcon = document.querySelector('.arrow-icon');
-
-  // Toggle content visibility
-  content.classList.toggle('open');
-
-  // Change the arrow direction by toggling class
-  if (content.classList.contains('open')) {
-      arrowIcon.classList.remove('fa-chevron-down');
-      arrowIcon.classList.add('fa-chevron-up');
-  } else {
-      arrowIcon.classList.remove('fa-chevron-up');
-      arrowIcon.classList.add('fa-chevron-down');
-  }
-});
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#navbar'
+})
